2082bda086ac7b34b30d959df7ead63117ad94c1
w3prodigy <w3prodigy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-10 16:06:48 +0000

    updated all library locators
    git-svn-id: https://plugins.svn.wordpress.org/google-hosted-ajax-libraries/trunk@407903 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- google-ajax-lib-hosting-plugin.php
+++ google-ajax-lib-hosting-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Google Hosted AJAX Libraries
 Plugin URI: http://w3prodigy.com/wordpress-plugins/google-hosted-ajax-libraries/
 Description: This plugin changes the internal WordPress script queues for the following AJAX Libraries: jQuery, jQuery UI, Prototype, Scriptaculous, MooTools, Yahoo! User Interface, Dojo, SWFObject, Ext-Core, Chrome Frame and WebFont Loader.
-Version:  1.3
+Version:  1.3.1
 Author: Jay Fortner
 Author URI: http://w3prodigy.com
 License: GNU General Public License v2
@@ -16,17 +16,17 @@ class w3p_Google_Hosted_AJAX_Libraries {
 	function w3p_Google_Hosted_AJAX_Libraries()
 	{
 		$this->settings = array(
-			'jquery' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js',
-			'jquery-ui-core' => 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js',
-			'prototype' => 'http://ajax.googleapis.com/ajax/libs/prototype/1.6.1.0/prototype.js',
-			'scriptaculous' => 'http://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.3/scriptaculous.js',
-			'moo-tools' => 'http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js',
-			'dojo' => 'http://ajax.googleapis.com/ajax/libs/dojo/1.4.1/dojo/dojo.xd.js',
+			'jquery' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
+			'jquery-ui-core' => 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js',
+			'prototype' => 'http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js',
+			'scriptaculous' => 'http://ajax.googleapis.com/ajax/libs/scriptaculous/1.9.0/scriptaculous.js',
+			'moo-tools' => 'http://ajax.googleapis.com/ajax/libs/mootools/1.3.2/mootools-yui-compressed.js',
+			'dojo' => 'http://ajax.googleapis.com/ajax/libs/dojo/1.6.1/dojo/dojo.xd.js',
 			'swfobject' => 'http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js',
-			'yui' => 'https://ajax.googleapis.com/ajax/libs/yui/3.3.0/build/yui/yui-min.js',
+			'yui' => 'http://ajax.googleapis.com/ajax/libs/yui/3.3.0/build/yui/yui-min.js',
 			'extcore' => 'http://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js',
 			'chrome-frame' => 'http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js',
-			'webfont-loader' => 'http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
+			'webfont-loader' => 'http://ajax.googleapis.com/ajax/libs/webfont/1.0.21/webfont.js'
 			);
 		
 		add_action('init', array(&$this,'init'));
